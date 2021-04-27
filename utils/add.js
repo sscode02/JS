d0a9ae92870a3ef5fd830797864fd47b2c90a114
Ben <you@example.com>
2021-04-27 17:46:19 +0800

    feat: add currie

--- utils/add.js
+++ utils/add.js
@@ -1,19 +1,14 @@
-
-
 function add(...nums) {
   function helper(...nums2) {
     return add(...nums, ...nums2)
   }
+
   helper.sumOf = function () {
     console.log(nums.reduce((prev, cur) => prev + cur))
   }
+
   return helper
 }
-
-
-
-
-
 
 add(1).sumOf() // 1
 add(1, 2).sumOf() // 3
