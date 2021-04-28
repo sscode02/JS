862df0bcfeac12635863d604d41a236e7f05004d
Ben <you@example.com>
2021-04-29 00:21:06 +0800

    refactor: Change the function to a class

--- datastructure/queue.js
+++ datastructure/queue.js
@@ -1,30 +1,27 @@
-function Queue() {  //队列先进先出
-  this.items = []
+class Queue {
+  items = []
 
-  Queue.prototype.enQueue = function (element) { //加入队列
-    this.items.push(element)
+  enQueue(element) { //加入队列
+    return this.items.push(element)
   }
 
-  Queue.prototype.deQueue = function () { //取出并删除-队列值
+  deQueue() { //从队列中取出值并删除
     return this.items.shift()
   }
 
-  Queue.prototype.front = function () { //查看第一个值
+  front() {
     return this.items[0]
   }
 
-  Queue.prototype.size = function () { //计算队列长度
+  isEmpty() {
+    return this.items.length === 0
+  }
+
+  size() {
     return this.items.length
   }
 
-  Queue.prototype.toString = function () { //输出队列值
-    let resultString = ""
-
-    for (let i = 0; i < this.items.length; i++) {
-      resultString += this.items[i] + ""
-    }
-
-    return resultString
+  toString() {
+    return this.items.join(' ')
   }
-
 }
