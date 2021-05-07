075b70f4632f16acdf92418b5e57f6091cdd52df
Ben <you@example.com>
2021-05-07 11:07:31 +0800

    feat: add union and intersection and difference

--- datastructure/set.js
+++ datastructure/set.js
@@ -30,5 +30,43 @@ class _Set {
   toString() {
     return Object.keys(this.items).join('->')
   }
+
+  union(otherSet) {
+    const resultSet = new _Set()
+    const arr = this.values().concat(otherSet.values()) //把两个数组连接成一个再添加 add方法会去重
+
+    for (let i = 0; i < arr.length; i++) {
+      resultSet.add(arr[i])
+    }
+
+    return resultSet
+  }
+
+  intersection(otherSet) {
+    const resultSet = new _Set()
+    const arr = this.values().concat(otherSet.values())
+
+    for (let i = 0; i < arr.length; i++) {
+      if (this.items.hasOwnProperty(arr[i]) && otherSet.items.hasOwnProperty(arr[i])) {
+        resultSet.add(arr[i])
+      }
+    }
+
+    return resultSet
+  }
+
+  difference(otherSet) {
+    const resultSet = this.values()
+    const arr = otherSet.values()
+
+    for (let i = 0; i < arr.length; i++) {
+      const index = resultSet.indexOf(arr[i])
+      if (index > 0) {
+        resultSet.splice(index, 1)
+      }
+    }
+
+    return resultSet
+  }
 }
 
